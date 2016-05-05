3c7aece4078ab792de49e8be018ae699c3323f64
diegosaraujo <diesaraujo@gmail.com>
2016-05-05 17:05:32 -0300

    Created Template for index

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-   return HttpResponse('Bem-vindo ao ConnectedIn')
+   return render (request, 'index.html')
 
 
